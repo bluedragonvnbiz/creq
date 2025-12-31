af97297c2901c0b967184e559f2b8eecfa18e5b2
tanpv01 <tan@bluedragonvn.com>
2025-12-31 11:59:00 +0700

    UI admin _campaign_ influencer detail

--- admin/pages/campaigns/edit.php
+++ admin/pages/campaigns/edit.php
@@ -2,7 +2,7 @@
 <?php get_template_part("admin/template-parts/layouts/topbar","",["title" => "캠페인 관리"]) ?>
 <div class="border bg-white d-flex align-items-center justify-content-between camp-header">
 	<div class="d-flex gap-15">
-		<img class="object-fit-cover" src="http://localhost/creq/wp-content/themes/creq/assets/images/demo/demo-1.jpg" alt="" style="width:45px;height: 45px;border-radius: 5px;">
+		<img class="object-fit-cover" src="<?= IMAGES_URL ?>/demo/demo-1.jpg" alt="" style="width:45px;height: 45px;border-radius: 5px;">
 		<div class="d-flex flex-column fs-12 justify-content-center">
 			<strong class="fw-semibold">캠페인명</strong>
 			<span>제품명</span>
